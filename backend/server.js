eccd698e819e3476c3780c75cf6656c8801878e5
Noah <admin@NoahsShitbox.local>
2023-11-24 15:18:34 +0200

    Done Woohooo

---
+++ backend/server.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const app = express()
+const cors = require("cors")
+const { exec } = require('child_process');
+const username_set = "spectrum"
+const password_set = "paradox"
+
+const corsOptions = {
+  origin: "*",
+};
+
+let user_exists = false
+let password_exists = false
+
+function verify_connect(username, password) {
+  if(username == username_set){
+    user_exists = true
+    console.log("Username Good!")
+  }
+  if(password == password_set){
+    password_exists = true
+    console.log("Password Good!")
+  }
+
+  return 0
+}
+function run_command(command) {
+  return new Promise((resolve, reject) =>{
+    exec(command, (error, stdout, stderr) => {
+      if(error){
+        console.log("Failed to execute command")
+        reject(error)
+      }
+      else {
+        resolve(stdout.trim())
+      }
+    })
+  })
+}
+app.use(cors(corsOptions))
+
+app.get('/', (req, res) => {
+  res.send("All Good!")
+})
+app.get('/connect/', (req, res) => {
+  verify_connect(req.query.username, req.query.password)
+  console.log("attempted to connect with name", req.query.username, "and password", req.query.password)
+})
+
+app.get('/disconnect/', (req, res) => {
+  console.log("Disconnecting from server!")
+})
+
+app.get('/status/', (req, res) => {
+  console.log("Checking status...")
+  if(user_exists && password_exists){
+    console.log("Logged in succesfully as:", username_set)
+    res.send("Connected")
+  }
+  else {
+    console.log("incorrect username or password")
+  }
+})
+
+app.get('/command/', async (req, res) => {
+  console.log("Attempting to run command:", req.query.command)
+  if (user_exists && password_exists){
+  console.log("Running: ", req.query.command)
+  let output = await run_command(req.query.command)
+  console.log(output)
+  res.send(output)
+  }
+  else {
+    console.log("Not Connected!")
+  }
+})
+
+app.listen(5000, () => {
+  console.log("Im live on port 5000")
+})
---
+++ index.js
@@ -0,0 +1,66 @@
+const hostname = document.getElementById("hostname");
+const password = document.getElementById("password");
+const shell = document.getElementById("shell")
+const label = document.getElementById("test")
+let state = 0
+
+
+async function request_connect(){
+  let user = hostname.value
+  let pass = password.value
+  let url = 'http://127.0.0.1:5000/connect/?username=' + user + '&password=' + pass
+
+  fetch_api(url) 
+  url = 'http://127.0.0.1:5000/status/'
+  let response = await fetch_api(url)
+  console.log(response)
+  if(response == "Connected"){
+    console.log("We are succesfully connected")
+  }
+
+  
+  return 0
+}
+async function fetch_api(url){
+  console.log('connecting to:', url)
+  let response = await fetch(url, {
+    mode: 'cors'
+  })
+  
+  if(!response.ok){
+    console.error("Invalid HTTP request")
+  }
+  console.log(response.status)
+  let text = await response.text()
+
+  return text
+}
+async function disconnect(){
+
+  let url = 'http://127.0.0.1:5000/disconnect/'
+  
+  fetch_api(url, mode='no-cors')
+
+  return 0
+}
+async function command(){
+  console.log("sending command:", shell.value)
+  let url = 'http://127.0.0.1:5000/command/?command=' + shell.value
+
+  let output = await fetch_api(url)
+
+  label.innerHTML = output
+
+  return 0
+}
+
+//while(true){
+//  if (state == 1){
+//    console.log("Connected Succesfully!")
+//    state := 
+//  }
+//}
+
+// endpoint /connect/?username=name&password=password
+// should return 1 0r 0 depending on status of connection
+
